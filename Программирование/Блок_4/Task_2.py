345374d461ebe111a6d4d7c06724c5db8975dcc7
SlavaAlferov <slaventy228822@gmail.com>
2025-03-28 20:36:44 +0300

    Update Task_2.py

--- Программирование/Блок_4/Task_2.py
+++ Программирование/Блок_4/Task_2.py
@@ -1,1 +1,26 @@
+def reverse(input_str):
+    """
+    С клавиатуры поступает строка.
+    Необходимо вывести строку, где порядок слов в противоположном направлении.
+    Первое слово с заглавной буквы, остальные с маленькой. МЕЖДУ словами только ОДИН пробел.
+        Input: “hello world”
+        Output: "Hello world"
+        Input: "it was cool"
+        Output: "Cool was it"
+        Input: "good"
+        Output: "Good"
+    """
+    if not input_str.strip():
+        return ""
 
+    words = input_str.lower().split()
+    words.reverse()
+    return ' '.join(words).capitalize()
+
+
+def main():
+    input_str = input()
+    print(reverse(input_str))
+
+if __name__ == '__main__':
+    main()
